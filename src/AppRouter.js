01c3cc5a7253824c5e98d51154ebb1f202c284c3
Denis <denis.musoev@yandex.ru>
2024-06-19 12:33:30 +0400

    Update AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -37,25 +37,9 @@ const AnimatedRoutes = () => {
         animate: {opacity: 1},
         exit: {opacity: 0},
         transition: {duration: 0.3, delay: 0}
-
-
-
-
-
-
-
-
-
     };
 
     return (
-
-
-
-
-
-
-
         <>
             <AnimatePresence mode="wait">
                 {/*<div style={{position: 'absolute', width: '100%'}}>*/}
@@ -81,9 +65,6 @@ const AnimatedRoutes = () => {
                 {/*</div>*/}
             </AnimatePresence>
         </>
-
-
-
     )
 };
 
